17634c0c403704fc12c58257e5d5a745ccb5bf30
code-teacher1 <iamhonore@outlook.com>
2026-05-20 10:10:33 -0400

    added comments to explain codes

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
-#include <stdio.h>
+#include <stdio.h> // this is like bringing a library that allows you to use printf function
+
+// here is the program codes are written in the main functioin 
 int main(){
+
+    //printing something
     printf("Hello, World!\n");
+    //we use return 0 as we said that our program will return an integer writing int main 
     return 0;
 }
