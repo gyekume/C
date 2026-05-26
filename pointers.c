948730d482c916873b3da655f8125c28eb1cd4f0
code-teacher1 <iamhonore@outlook.com>
2026-05-26 22:36:32 +0200

    created a pointers.c file to show what are pointers and what you can do with them

---
+++ pointers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int number = 45;
+    int *p = &number; //this is how we declare a pointer that points to an address of a valiable that stores a value
+    printf("%d \n",number);
+    printf("%d \n",p); // printing an address
+
+    printf("%d \n",*p);  //showing how you can access the value using the pointer that points to an address
+
+    *p  =  30;
+    printf("%d",number); // this shows that we have changed the value of the number using the pointer that links to an address we are
+    return 0;
+}
