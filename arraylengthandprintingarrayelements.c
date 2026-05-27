e05b61ee1b3267dad80e91f46baf301111ee7283
code-teacher1 <iamhonore@outlook.com>
2026-05-27 17:09:13 +0200

    added autosave in my vscode as the code editor and add line 8 to test it and also i used multiline comments from line 7 and 7 as are explaining one thing

--- arraylengthandprintingarrayelements.c
+++ arraylengthandprintingarrayelements.c
@@ -3,8 +3,9 @@ int main(){
     int arr[] = {1,2,3,4,5};
     printf("%d \n",sizeof(arr)/sizeof(arr[0])); // this is how we can find the length of an array in C
    
-    // why this way is because sizeof turn the memory size of the variable size of array turned 20 bytes as one int is 4 bytes then 
-    // we divide it by the size of one element in the array which is 4 bytes then we get the length of the array which is 5
+    /* why this way is because sizeof turn the memory size of the variable size of array turned 20 bytes as one int is 4 bytes then 
+     we divide it by the size of one element in the array which is 4 bytes then we get the length of the array which is 5*/
+     printf("the length of the array is: %d \n",sizeof(arr)/sizeof(arr[0]));
 
     //now we are also going to see how to print the members of an array using a loop
     printf("the members of the array are: \n");
