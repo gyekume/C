10da9b4237005ce3378e6de17a2b39d70c9d0ecc
code-teacher1 <iamhonore@outlook.com>
2026-05-29 20:52:41 +0200

    added a file to explain characters

---
+++ characters.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    // character can be a  letter whether capital or small or a symbol and it's memory it takes 1 byte recall int use 4 bytes
+
+    //declaring  a character we use single quotes  not double quotes
+
+    char firstAlphabeticLetter = 'A';
+
+    //printing a character
+
+    printf("%c",firstAlphabeticLetter);
+
+    return 0;
+}
