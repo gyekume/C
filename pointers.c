ccb4e39c2253aba07149ae58a046eab4e73d8820
code-teacher1 <iamhonore@outlook.com>
2026-05-29 21:58:53 +0200

    printig address using %p as format specifer

--- pointers.c
+++ pointers.c
@@ -3,7 +3,11 @@ int main(){
     int number = 45;
     int *p = &number; //this is how we declare a pointer that points to an address of a valiable that stores a value
     printf("%d \n",number);
-    printf("%d \n",p); // printing an address
+    printf("%d \n",p); // printing an address in decimal number using integer 
+
+    // printing and address using a p format specifier in c which use hexadecimal to represent storage address
+
+    printf("%p",p);
 
     printf("%d \n",*p);  //showing how you can access the value using the pointer that points to an address
 
