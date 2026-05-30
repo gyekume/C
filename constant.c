d5221ff5002c62fe3d2790aa11aeff8fdecd8411
code-teacher1 <iamhonore@outlook.com>
2026-05-30 21:37:07 +0200

    added a file explaining constant values in c

---
+++ constant.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define PI 3.14; // using define keyword don't forget the radder
+
+const  float GRAVITY = 9.8;
+
+
+int main(){
+    //constant are variables with values that are constant that will not change we can define them in two ways for example 
+    // pi is a  constant , gravity so you have to declare them in their way you can use define keyword and const and it is better
+    //to define them before int main to be used every where.
+
+    //constants should be written in capital letters
+
+    //using them [constants]
+    float diameter;
+    printf("Enter the diameter of your circle  in metres : ");
+    scanf("%f",&diameter);
+    float circumfrence  = diameter * PI;
+    printf("The circumfrence of your circle is %f m",circumfrence);
+    
+    return 0;   
+    
+}
