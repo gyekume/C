9ffc9f471188b57294cc3c105e58e796237e90d5
code-teacher1 <iamhonore@outlook.com>
2026-05-30 21:49:00 +0200

    improved constant explanations

--- constant.c
+++ constant.c
@@ -2,9 +2,6 @@
 
 #define PI 3.14; // using define keyword don't forget the radder
 
-const  float GRAVITY = 9.8;
-
-
 int main(){
     //constant are variables with values that are constant that will not change we can define them in two ways for example 
     // pi is a  constant , gravity so you have to declare them in their way you can use define keyword and const and it is better
@@ -17,7 +14,19 @@ int main(){
     printf("Enter the diameter of your circle  in metres : ");
     scanf("%f",&diameter);
     float circumfrence  = diameter * PI;
-    printf("The circumfrence of your circle is %f m",circumfrence);
+    printf("The circumfrence of your circle is %f m \n",circumfrence);
+
+
+
+
+    const  float GRAVITY = 9.8;
+    float mass;
+    printf("Enter the mass in kg  of your body we find it's force : ");
+    scanf("%f",&mass);
+    float force = mass * GRAVITY;
+
+    printf("the force is %f",force);
+
     
     return 0;   
     
